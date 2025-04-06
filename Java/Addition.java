3aee0165a78a28abaf62ecc66b1abc5fb9609e80
shibashis-dora <shibashisdora@gmail.com>
2025-04-06 14:09:44 +0530

    Addition.java class updated.

--- Java/Addition.java
+++ Java/Addition.java
@@ -1,8 +1,13 @@
 public class Addition {
+
+  private static int addition(int a, int b) {
+    return a+b;
+  }
+
   public static void main (String[] args) {
     int a = 5;
     int b = 6;
 
-    System.out.println("Addition of a and b is "+ (a+b));
+    System.out.println("Addition of a and b is "+ addition(a,b));
   }
 }
