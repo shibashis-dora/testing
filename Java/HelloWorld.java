06e3d07ecf0e29ce2d5d781b46006a6102bb25e8
shibashis-dora <shibashisdora@gmail.com>
2025-04-06 12:39:13 +0530

    Added a java folder and a java file.

---
+++ Java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is my fisrt program");
+        System.out.println("Learning git and github.");
+    }
+}
